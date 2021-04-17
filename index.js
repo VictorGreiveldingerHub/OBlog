f5056854a3357761505b515e0be0f5881e3eda56
Greiveldinger Victor <victorgreiveldinger@gmail.com>
2021-04-17 15:33:44 +0200

    Mise en place CSS, création des dossiers, router, views, et factorisation des views

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const port = 3000;
-
 const app = express();
+const router = require('./modules/router');
 
-app.get('/', (req, res) => {
-    res.send('Hello debut de projet');
-});
+app.use(express.static('public'));
+app.use(router);
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 
 app.listen(port, (req, res) => {
     console.log(`Server listening on port ${port}`);
